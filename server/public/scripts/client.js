2146571d15cdad73852d3c4066909c61579b86c9
Kevin Dexter <kevin.dexter@comcast.net>
2018-04-19 11:27:30 -0500

    add records route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,19 @@ $(document).ready(onReady);
 
 function onReady () {
   console.log ('jquery-3.3.1.min.js has been loaded');
+  loadRecords();
+}
+
+function loadRecords() {
+  $.ajax({
+    type: 'GET',
+    url: '/records'
+  })
+  .then(function(response) {
+    $('body').append('<ul id="records"></ul>');
+    response.forEach(record => {
+      let recordString = `${record.title} was released in ${record.year} by ${record.artist} and cost $${record.cost.toLocaleString()} to make.`;
+      $('#records').append(`<li>${recordString}</li>`);
+    });
+  })
 }
